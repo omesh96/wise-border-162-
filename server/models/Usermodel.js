407a3ab0cec66e737580ee075c2920cce482e83b
Omesh Dubey <105977022+omesh96@users.noreply.github.com>
2023-02-21 14:45:53 +0530

    start

---
+++ server/models/Usermodel.js
@@ -0,0 +1,30 @@
+const mongoose=require("mongoose")
+
+const UserSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+       
+    },
+    password:{
+        type:String,
+        required:[true, "Please provide a Password"],
+        unique:false
+    },
+    email:{
+        type:String,
+        required:[true, "please provide a unique email"],
+        unique:true
+    },
+   
+    mobile:{
+        type:Number,
+        required:[true, "Please provide a Mobile Number"]
+    },
+    address:{type:String}
+   
+})
+
+const UserModel=mongoose.model("user",UserSchema)
+
+module.exports={UserModel}
