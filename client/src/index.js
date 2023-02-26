733db74918803c67a851731b175138d86b950678
snehilagrahari <sag21062001@outlook.com>
2023-02-26 19:41:32 +0530

    Cart Order

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -24,28 +23,3 @@ root.render(
     </ChakraProvider>
   </Provider>
 );
-=======
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react'
-import { Provider } from 'react-redux';
-import { store } from './Redux/store';
-import AuthContextProvider from './Admin/context/Allcontext';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Provider store={store}>
-<AuthContextProvider>
-<ChakraProvider>
-   <BrowserRouter>
-   <App />
-   </BrowserRouter>
-   </ChakraProvider>
-</AuthContextProvider>
-</Provider>
-);
->>>>>>> b5e5ac6baa1cff8035212ebc2515e0ded4248578
