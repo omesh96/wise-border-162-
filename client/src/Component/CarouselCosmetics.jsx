e62c773ca9bd1346e4e460f095516ff61fe33b8c
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-07-04 11:21:38 -0700

    Update CarouselCosmetics.jsx

--- client/src/Component/CarouselCosmetics.jsx
+++ client/src/Component/CarouselCosmetics.jsx
@@ -39,6 +39,7 @@ const settings = {
   initialSlide: 0,
 };
 
+
 export default function CarouselCosmetics() {
   // As we have used custom buttons, we need a reference variable to
   // change the state
